916d0c446061a6d5a962b53eaa7a3b5fbb009266
GorillaDunk <ehdgus5289@gmail.com>
2023-10-10 15:42:02 +0900

    email sending half-implemented

--- ThreeThirty_BE/src/main/java/com/example/ThreeThirty_BE/controller/MailController.java
+++ ThreeThirty_BE/src/main/java/com/example/ThreeThirty_BE/controller/MailController.java
@@ -1,25 +1,49 @@
 package com.example.ThreeThirty_BE.controller;
 
-import com.example.ThreeThirty_BE.dto.login.UsermailDto;
-import com.example.ThreeThirty_BE.service.MailService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ThreeThirty_BE.domain.EmailMessage;
+import com.example.ThreeThirty_BE.dto.login.EmailPostDto;
+import com.example.ThreeThirty_BE.dto.login.EmailResponseDto;
+import com.example.ThreeThirty_BE.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@AllArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MailController {
-    private final MailService mailService;
 
-    @GetMapping("/mail")
-    public String dispMail(){
-        return "mail.html";
+    private final EmailService emailService;
+
+
+    // 임시 비밀번호 발급
+    @PostMapping("/password")
+    public ResponseEntity sendPasswordMail(@RequestBody EmailPostDto emailPostDto) {
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(emailPostDto.getEmail())
+                .subject("[SAVIEW] 임시 비밀번호 발급")
+                .build();
+
+        emailService.sendMail(emailMessage, "password");
+
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/mail")
-    public void execMail(UsermailDto mailDto){
-        mailService.mailSend(mailDto);
+    // 회원가입 이메일 인증 - 요청 시 body로 인증번호 반환하도록 작성하였음
+    @PostMapping("/email")
+    public ResponseEntity sendJoinMail(@RequestBody EmailPostDto emailPostDto) {
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(emailPostDto.getEmail())
+                .subject("[SAVIEW] 이메일 인증을 위한 인증 코드 발송")
+                .build();
+
+        String code = emailService.sendMail(emailMessage, "email");
+
+        EmailResponseDto emailResponseDto = new EmailResponseDto();
+        emailResponseDto.setCode(code);
+
+        return ResponseEntity.ok(emailResponseDto);
     }
 }
---
+++ ThreeThirty_BE/src/main/java/com/example/ThreeThirty_BE/dto/login/EmailResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.ThreeThirty_BE.dto.login;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailResponseDto {
+    private String code;
+}
